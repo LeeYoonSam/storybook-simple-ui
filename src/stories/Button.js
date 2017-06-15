19ae39413514a4ff8e08f73d1f970c7f4bb7aa72
YS <yoonsam86@gmail.com>
2017-06-15 23:18:48 +0900

    add button style

---
+++ src/stories/Button.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Radium from 'radium';
+
+const Button = ({fixColor, hoverColor, fontSize, padding, children, onClick }) => (
+  <button
+    style={{
+      border: '1px solid ' + fixColor,
+      backgroundColor: 'transparent',
+      color: fixColor,
+      padding: padding,
+      fontSize: fontSize,
+      ':hover': {
+        backgroundColor: fixColor,
+        color: hoverColor
+      }
+    }}
+    onClick={onClick}
+  >
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  fixColor: React.PropTypes.string.isRequired,
+  hoverColor: React.PropTypes.string.isRequired,
+  fontSize: React.PropTypes.number,
+  padding: React.PropTypes.number,
+  children: React.PropTypes.string.isRequired,
+  onClick: React.PropTypes.func,
+};
+
+Button.defaultProps = {
+  fontSize: 15,
+  padding: 20
+}
+
+export default Radium(Button);
---
+++ src/stories/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf, action, linkTo } from '@kadira/storybook';
+import Button from './Button';
+import Welcome from './Welcome';
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <div style={{backgroundColor:'gray', padding:'10'}}>
+      <div>
+        <Button fixColor="#000" hoverColor="#fff" onClick={action('clicked')}>Hello Button</Button>
+      </div>
+      <div>
+        <Button fixColor="#fff" hoverColor="#000" onClick={action('clicked')}>Hello Button</Button>
+      </div>
+      <div>
+        <Button fixColor="#fff" hoverColor="#000" fontSize="30" padding="50" onClick={action('clicked')}>Hello Button</Button>
+      </div>
+    </div>
+  ))
+  .add('with some emoji', () => (
+    <Button fixColor="#000" hoverColor="#fff" onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
